84c4613f81aa7b3797442752b25a5a5b90793268
iancarlos9 <145473426+iancarlos9@users.noreply.github.com>
2023-10-12 12:50:46 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('about').addEventListener('click', function () {
+    window.location.href = 'sobre.html';
+});
+
+document.getElementById('education').addEventListener('click', function () {
+    window.location.href = 'formacoes.html';
+});
+
+document.getElementById('portfolio').addEventListener('click', function () {
+    window.location.href = 'portfolio.html';
+});
+
+document.getElementById('contact').addEventListener('click', function () {
+    window.location.href = 'contato.html';
+});
